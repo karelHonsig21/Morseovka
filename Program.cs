5b4e7900e29797de920d14f83c053d1706b08161
karelHonsig21 <88852747+karelHonsig21@users.noreply.github.com>
2023-02-22 12:22:45 +0100

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿//vstup uživatele
+
+
+
+//pole - abeceda + morseovka
+string abeceda = "abcdefghijklmnopqrstuvwxyz";
+string[] morseovka = {".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....",
+                          "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-",
+                          "...-", ".--", "-..-", "-.--", "--.."};
+//přeložení abeceda -> morseovka
+
+
+
+
+
+
+//výpis zakódované zprávy
